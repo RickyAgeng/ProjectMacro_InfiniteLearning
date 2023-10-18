74853b13d8ee7ae14daf4ea9f4a6551645c4e47d
Ricky Ageng S <rickyageng01@gmail.com>
2023-10-18 23:47:36 +0700

    Update Pengenalan

--- app/src/main/java/com/example/eduplay/Pengenalan.kt
+++ app/src/main/java/com/example/eduplay/Pengenalan.kt
@@ -1,12 +1,49 @@
 package com.example.eduplay
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 
-class Pengenalan : AppCompatActivity() {
+class Pengenalan : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pengenalan)
+
+        val btnBtn1 : ImageView = findViewById(R.id.btn1)
+        btnBtn1.setOnClickListener(this)
+        val btnBtn2 : ImageView = findViewById(R.id.btn2)
+        btnBtn2.setOnClickListener(this)
+        val btnBtn3 : ImageView = findViewById(R.id.btn3)
+        btnBtn3.setOnClickListener(this)
+        val btnBtn4 : ImageView = findViewById(R.id.btn4)
+        btnBtn4.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when(v?.id) {
+                R.id.btn1 -> {
+                    val intent = Intent(this@Pengenalan, Pengenalan_Buah1::class.java)
+                    startActivity(intent)
+                }
+                R.id.btn1 -> {
+                    val intent = Intent(this@Pengenalan, Pengenalan_Hewan1::class.java)
+                    startActivity(intent)
+                }
+                R.id.btn1 -> {
+                    val intent = Intent(this@Pengenalan, Pengenalan_Warna1::class.java)
+                    startActivity(intent)
+                }
+                R.id.btn1 -> {
+                    val intent = Intent(this@Pengenalan, Pengenalan_Pancasila::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/eduplay/Pengenalan_Pancasila.kt
+++ app/src/main/java/com/example/eduplay/Pengenalan_Pancasila.kt
@@ -1,11 +1,31 @@
 package com.example.eduplay
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 
-class Pengenalan_Pancasila : AppCompatActivity() {
+class Pengenalan_Pancasila : AppCompatActivity(), View.OnClickListener {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pengenalan_pancasila)
+
+        val btnNext1 : ImageView = findViewById(R.id.iv_next)
+        btnNext1.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.iv_next -> {
+                val intent = Intent(this@Pengenalan_Pancasila, Pengenalan_Pancasila2::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
