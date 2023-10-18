9eba06fe7e05b4a380a01ae93caf528860ee199d
Your Name <you@example.com>
2023-10-18 17:26:38 +0700

    fitur bernyanyi

--- app/src/main/java/com/example/eduplay/Menulis_A.kt
+++ app/src/main/java/com/example/eduplay/Menulis_A.kt
@@ -3,9 +3,9 @@ package com.example.eduplay
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Menulis_A : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bernyanyi)
+        setContentView(R.layout.activity_menulis)
     }
 }
