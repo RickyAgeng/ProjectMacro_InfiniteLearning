9757b953382615ba9f81b520ce9c0c11eadb5c13
Hasan nudin <hasannudin11111@gmail.com>
2023-10-19 11:38:24 +0700

    Berhitung

---
+++ app/src/main/java/com/example/eduplay/BerhitungActivity_B.kt
@@ -0,0 +1,11 @@
+package com.example.eduplay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BerhitungActivity_B : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_berhitung_b)
+    }
+}
